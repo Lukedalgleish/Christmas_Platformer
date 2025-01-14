26a0fe1ee57542343843a7449fa03a0cde21d930
luke dalgleish <lukedalgleish07@gmail.com>
2025-01-14 21:03:35 +0000

    Camera now follows the player
    I also moved some scripts to the correct files

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject player; 
+    private Vector3 offset = new Vector3(0, 0, -1);      // Offset from the player
+    private float smoothSpeed = 0.125f;  // How smooth the camera moves
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 desiredPosition = player.transform.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
